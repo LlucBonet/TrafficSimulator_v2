d1b8060ed3b5e65c4e55fb19c081e70041bad9be
maria <maria@192.168.1.34>
2020-04-15 23:35:51 +0200

    adaptacion TrafficSimulator a la GUI

--- TrafficSimulator/src/simulator/model/TrafficSimulator.java
+++ TrafficSimulator/src/simulator/model/TrafficSimulator.java
@@ -1,5 +1,6 @@
 package simulator.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.json.JSONObject;
@@ -11,24 +12,26 @@ public class TrafficSimulator implements Observable<TrafficSimObserver>{
 	protected RoadMap _map;
 	protected List<Event> _eventList;
 	protected int _simulatedTime;
-	
+	protected TrafficSimObserver _o;
+	protected List<TrafficSimObserver> _observers;
 	
 	public TrafficSimulator() {
 		_map = new RoadMap();
 		_eventList = new SortedArrayList<Event>();
 		_simulatedTime = 0;
+		_observers = new ArrayList<TrafficSimObserver>();
 	}
 	
 	public void addEvent(Event e) {
 		_eventList.add(e);
-		onEventAdded(_map, _eventList, e, _simulatedTime);
+		_o.onEventAdded(_map, _eventList, e, _simulatedTime);
 	}
 	
 	public void advance() throws Exception {
 		
 		_simulatedTime++; //paso 1
 		
-		onAdvanceStart(_map, _eventList, _simulatedTime);
+		_o.onAdvanceStart(_map, _eventList, _simulatedTime);
 		
 		while (!(_eventList.isEmpty()) && _eventList.get(0).getTime() == _simulatedTime) {//j < _eventList.size()
 			_eventList.get(0).execute(_map);
@@ -44,14 +47,14 @@ public class TrafficSimulator implements Observable<TrafficSimObserver>{
 			_map.getRoads().get(i).advance(_simulatedTime);
 		}//paso 4
 		
-		onAdvanceEnd(_map, _eventList, _simulatedTime);
+		_o.onAdvanceEnd(_map, _eventList, _simulatedTime);
 	}
 	
 	public void reset() {
 		_map.reset();
 		_eventList.clear();
 		_simulatedTime = 0;
-		onReset(_map, _eventList, _simulatedTime);
+		_o.onReset(_map, _eventList, _simulatedTime);
 	}
 	
 	public JSONObject report() {
@@ -67,11 +70,12 @@ public class TrafficSimulator implements Observable<TrafficSimObserver>{
 	//IMPLEMENTS OBSERVABLE<TRAFFICSIMOBSERVER>
 	@Override
 	public void addObserver(TrafficSimObserver o) {
-		
+		_observers.add(o);
+		_o.onRegister(_map, _eventList, _simulatedTime);
 	}
 
 	@Override
 	public void removeObserver(TrafficSimObserver o) {
-		
+		_observers.remove(o);
 	}
 }
